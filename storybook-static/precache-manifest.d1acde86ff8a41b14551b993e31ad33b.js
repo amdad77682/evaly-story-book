eb6fd7da51f8c698aaa2dc8830081db64c8b1591
MD. Amdadul Haque <md.amdadulhaque@MDs-MacBook-Pro.local>
2020-07-20 02:10:43 +0600

    story book added

---
+++ storybook-static/precache-manifest.d1acde86ff8a41b14551b993e31ad33b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1073b3c6070d1040fa7d7d9f19d93f",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.36574e9c23cbda327aee.bundle.js"
+  },
+  {
+    "url": "runtime~main.36574e9c23cbda327aee.bundle.js"
+  },
+  {
+    "url": "vendors~main.36574e9c23cbda327aee.bundle.js"
+  },
+  {
+    "url": "vendors~main.36574e9c23cbda327aee.bundle.js.LICENSE.txt"
+  }
+]);
